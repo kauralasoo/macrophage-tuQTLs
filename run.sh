9e5b7613f2eb4a3e7b0f5cdb1de18a46a0f37ffa
Kaur Alasoo <kaur.alasoo@gmail.com>
2017-06-04 12:40:08 +0300

    Rerun quantification on the acLDL dataset

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #Quantify gene and transcript expression
 snakemake --cluster scripts/snakemake_submit.py -np -s quantify_transcription.snakefile processed/salmonella/out.txt --jobs 100 --configfile configs/salmonella_trQTL_config.yaml
+snakemake --cluster scripts/snakemake_submit.py -np -s quantify_transcription.snakefile processed/salmonella/out.txt --jobs 100 --configfile configs/acLDL_trQTL_config.yaml

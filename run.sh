0ec23c532a502d77518408332c6770932ec0120e
Kaur Alasoo <kaur.alasoo@gmail.com>
2017-06-05 15:56:39 +0300

    Import acLDL leafcutter data

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ snakemake --cluster scripts/snakemake_submit.py -np -s quantify_transcription.sn
 #Import transcript quants for acLDL data
 echo "test" | python ~/software/utils/submitJobs.py --MEM 8000 --jobname importTranscriptExpression --ncores 1 --queue normal --command "/software/R-3.4.0/bin/Rscript analysis/munge/acLDL_importTranscriptExpression.R"
 echo "test" | python ~/software/utils/submitJobs.py --MEM 8000 --jobname importReviseAnnotations --ncores 1 --queue normal --command "/software/R-3.4.0/bin/Rscript analysis/munge/acLDL_importReviseAnnotations.R"
+echo "test" | python ~/software/utils/submitJobs.py --MEM 8000 --jobname importLeafCutter --ncores 1 --queue normal --command "/software/R-3.4.0/bin/Rscript analysis/munge/acLDL_importLeafCutter.R"
 
 #Import transcript quants for Salmonella data
 echo "test" | python ~/software/utils/submitJobs.py --MEM 8000 --jobname importTranscriptExpression --ncores 1 --queue normal --command "/software/R-3.4.0/bin/Rscript analysis/munge/salmonella_importTranscriptExpression.R"

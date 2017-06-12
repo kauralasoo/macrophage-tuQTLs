b4a7b87e9cd0a0b0fb347cee72e60b03f44c6f17
Kaur Alasoo <kaur.alasoo@gmail.com>
2017-06-12 19:12:58 +0300

    Import coloc results

---
+++ analysis/coloc/condition_specific_coloc.R
@@ -0,0 +1,5 @@
+
+
+#Import coloc overlaps
+salmonella_olaps = readRDS("results/coloc/salmonella_GWAS_coloc_hits.rds")
+acLDL_olaps = readRDS("results/coloc/salmonella_GWAS_coloc_hits.rds")
---
+++ analysis/trQTLs/condition_specificity.R
@@ -0,0 +1,5 @@
+
+
+#Import minimal QTL p-values
+salmonella_qtls = readRDS("results/trQTLs/salmonella_trQTL_min_pvalues.rds")
+acldl_qtls = readRDS("results/trQTLs/acLDL_trQTL_min_pvalues.rds")

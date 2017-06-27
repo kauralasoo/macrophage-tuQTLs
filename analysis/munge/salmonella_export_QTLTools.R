bf3891c088af8a9728446704aba03af15ff595e3
Kaur Alasoo <kaur.alasoo@gmail.com>
2017-06-27 15:15:44 +0300

    Export featureCounts data for qtltools

--- analysis/munge/salmonella_export_QTLTools.R
+++ analysis/munge/salmonella_export_QTLTools.R
@@ -266,8 +266,70 @@ covariates_list = purrr::map(fc_list,
 saveFastqtlMatrices(covariates_list, output_path, file_suffix = "covariates_prop")
 
 
+#Export featureCounts cqns for qtl mapping
+se_gene = readRDS("results/SummarizedExperiments/salmonella_featureCounts.rds")
+event_metadata = rowData(se_gene)
+unique_genes = unique(event_metadata$gene_id)
 
+#Identify expressed genes from Salmon TPMs
+salmon_se = readRDS("results/SummarizedExperiments/salmonella_salmon_gene_abundances.rds")
+salmon_meta = rowData(salmon_se)
+salmon_dataset = salmon_se[salmon_meta[!salmon_meta$chr %in% c("X","Y","MT"),]$gene_id,]
 
+tpm_matrix = assays(salmon_dataset)$tpms
+mean_by_condition = calculateMean(tpm_matrix, colData(salmon_dataset) %>% as.data.frame(), factor = "condition_name")
+expressed_genes = mean_by_condition[apply(mean_by_condition, 1, max) > 1,]
 
+#Keep only expressed genes
+expressed_dataset = se_gene[intersect(rownames(expressed_genes), unique_genes),]
+
+#Extract lists for each condition
+condition_list = idVectorToList(c("naive","IFNg", "SL1344","IFNg_SL1344"))
+event_conditions = purrr::map(condition_list, ~extractConditionFromSummarizedExperiment(.,expressed_dataset))
+
+#Rename columns
+event_conditions_renamed = purrr::map(event_conditions, function(x){
+  colnames(x) = x$genotype_id
+  return(x)
+})
+
+#Construct gene positions for QTL mapping
+fastqtl_genepos = tbl_df2(rowData(expressed_dataset)) %>% 
+  dplyr::filter(gene_id %in% rownames(expressed_dataset)) %>%
+  dplyr::mutate(transcript_id = gene_id) %>%
+  constructQTLtoolsGenePos()
+output_path = "processed/salmonella/qtltools/input/featureCounts/"
+
+#Extract cqn matrices
+cqn_list = purrr::map(event_conditions_renamed, ~assays(.)$cqn)
+qtltools_cqn_list = purrr::map(cqn_list, prepareQTLtoolsMatrix, fastqtl_genepos)
+saveFastqtlMatrices(qtltools_cqn_list, output_path, file_suffix = "norm_prop")
+
+#Calculate covariates
+sample_meta = tbl_df2(colData(event_conditions_renamed$naive))
+covariates_list = purrr::map(cqn_list, 
+                             ~performPCA(., sample_meta, n_pcs = 6, feature_id = "genotype_id")$pca_matrix %>%
+                               dplyr::select(genotype_id, PC1, PC2, PC3, PC4, PC5, PC6) %>%
+                               fastqtlMetadataToCovariates())
+saveFastqtlMatrices(covariates_list, output_path, file_suffix = "covariates_prop")
+
+#Calculate fold-change
+sample_list = colnames(event_conditions_renamed$naive)
+fc_list = list(NI = cqn_list$IFNg[,sample_list] -
+                 cqn_list$naive[,sample_list],
+               NS = cqn_list$SL1344[,sample_list] -
+                 cqn_list$naive[,sample_list],
+               NIS = cqn_list$IFNg_SL1344[,sample_list] -
+                 cqn_list$naive[,sample_list])
+qtltools_list = purrr::map(fc_list, prepareQTLtoolsMatrix, fastqtl_genepos)
+saveFastqtlMatrices(qtltools_list, output_path, file_suffix = "norm_prop")
+
+#Calculate covariates for the fold-change
+sample_meta = tbl_df2(colData(event_conditions_renamed$naive))
+covariates_list = purrr::map(fc_list, 
+                             ~performPCA(., sample_meta, n_pcs = 6, feature_id = "genotype_id")$pca_matrix %>%
+                               dplyr::select(genotype_id, PC1, PC2, PC3, PC4, PC5, PC6) %>%
+                               fastqtlMetadataToCovariates())
+saveFastqtlMatrices(covariates_list, output_path, file_suffix = "covariates_prop")
 
 

80c01d4226b6503a412dfbb1c1c91107a78bc318
Kaur Alasoo <kaur.alasoo@gmail.com>
2017-06-28 14:01:58 +0300

    Make coverage plots for the IRF5 QTLs

--- analysis/figures/IRF5_example.R
+++ analysis/figures/IRF5_example.R
@@ -12,6 +12,19 @@ load_all("../wiggleplotr")
 se_featureCounts = readRDS("results/SummarizedExperiments/salmonella_featureCounts.rds")
 se_revised = readRDS("results/SummarizedExperiments/salmonella_salmon_reviseAnnotations.rds")
 
+#Import genotypes
+vcf_file = readRDS("results/genotypes/salmonella/imputed.86_samples.sorted.filtered.named.rds")
+
+#Set up bigwig metadata
+rna_meta_str1_df = wiggleplotrConstructMetadata(assays(se_featureCounts)$counts, 
+                                                tbl_df2(colData(se_featureCounts)),
+                                                "/Volumes/Ajamasin/bigwig/salmonella", 
+                                                bigWig_suffix = ".str1.bw")
+rna_meta_str2_df = wiggleplotrConstructMetadata(assays(se_featureCounts)$counts, 
+                                                tbl_df2(colData(se_featureCounts)),
+                                                "/Volumes/Ajamasin/bigwig/salmonella", 
+                                                bigWig_suffix = ".str2.bw")
+
 #Import GRangesList
 revised_granges = readRDS("results/annotations/reviseAnnotations.GRangesList.rds")
 
@@ -27,14 +40,53 @@ txs = dplyr::filter(tx_metadata, position == "upstream")
 tx_ranges = revised_granges[txs$transcript_id]
 plotTranscripts(tx_ranges)
 
-#Contained
+#Add genotype data
+track_data = wiggleplotrGenotypeColourGroup(rna_meta_str2_df, "rs3778754", vcf_file$genotypes, -1)
+filtered_tracks = dplyr::filter(track_data) %>% dplyr::filter(condition_name %in% c("naive"))
+
+#Make coverage plot
+coverage_plot = plotCoverage(exons = tx_ranges, 
+                             track_data = filtered_tracks, rescale_introns = TRUE,
+                             fill_palette = getGenotypePalette(), 
+                             plot_fraction = 0.2, heights = c(0.55,0.45), 
+                             return_subplots_list = FALSE, coverage_type = "line")
+ggsave("results/figures/IRF5_promoter_coverage.pdf", plot = coverage_plot, width = 6, height = 7)
+
+
+###### Contained ######
 txs = dplyr::filter(tx_metadata, position == "contained")
 tx_ranges = revised_granges[txs$transcript_id]
 plotTranscripts(tx_ranges)
 
-#Downstream
+#Add genotype data
+track_data = wiggleplotrGenotypeColourGroup(rna_meta_str2_df, "rs199508964", vcf_file$genotypes, -1)
+filtered_tracks = dplyr::filter(track_data) %>% dplyr::filter(condition_name %in% c("naive"))
+
+#Make coverage plot
+coverage_plot = plotCoverage(exons = tx_ranges, 
+                             track_data = filtered_tracks, rescale_introns = TRUE,
+                             fill_palette = getGenotypePalette(), 
+                             plot_fraction = 0.2, heights = c(0.55,0.45), 
+                             return_subplots_list = FALSE, coverage_type = "line")
+ggsave("results/figures/IRF5_RI_coverage.pdf", plot = coverage_plot, width = 6, height = 7)
+
+###### Downstream ######
 txs = dplyr::filter(tx_metadata, position == "downstream")
 tx_ranges = revised_granges[txs$transcript_id]
 plotTranscripts(tx_ranges)
+
+#Add genotype data
+track_data = wiggleplotrGenotypeColourGroup(rna_meta_str2_df, "rs10954213", vcf_file$genotypes, -1)
+filtered_tracks = dplyr::filter(track_data) %>% dplyr::filter(condition_name %in% c("naive"))
+
+#Make coverage plot
+coverage_plot = plotCoverage(exons = tx_ranges, 
+                             track_data = filtered_tracks, rescale_introns = TRUE,
+                             fill_palette = getGenotypePalette(), 
+                             plot_fraction = 0.2, heights = c(0.55,0.45), 
+                             return_subplots_list = FALSE, coverage_type = "line")
+ggsave("results/figures/IRF5_UTR_coverage.pdf", plot = coverage_plot, width = 6, height = 5)
+
+
 
 

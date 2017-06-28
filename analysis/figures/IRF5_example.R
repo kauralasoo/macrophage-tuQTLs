fe3cac2e0cf6f6d28451fd2dc2bb7d8b71c84480
Kaur Alasoo <kaur.alasoo@gmail.com>
2017-06-28 14:50:29 +0300

    Make IRF5 boxplots

--- analysis/figures/IRF5_example.R
+++ analysis/figures/IRF5_example.R
@@ -3,8 +3,8 @@ library("dplyr")
 library("ggplot2")
 library("purrr")
 library("tidyr")
-library("data.table")
 library("GenomicFeatures")
+library("SummarizedExperiment")
 load_all("../seqUtils/")
 load_all("../wiggleplotr")
 
@@ -12,8 +12,15 @@ load_all("../wiggleplotr")
 se_featureCounts = readRDS("results/SummarizedExperiments/salmonella_featureCounts.rds")
 se_revised = readRDS("results/SummarizedExperiments/salmonella_salmon_reviseAnnotations.rds")
 
+#Extract TPM matrix
+tpm_matrix = assays(se_revised)$tpm_ratios
+sample_meta = colData(se_revised) %>% tbl_df2()
+gene_meta = rowData(se_revised) %>% tbl_df2() %>%
+  dplyr::mutate(gene_id = transcript_id)
+
 #Import genotypes
 vcf_file = readRDS("results/genotypes/salmonella/imputed.86_samples.sorted.filtered.named.rds")
+variant_information = importVariantInformation("results/genotypes/salmonella/imputed.86_samples.variant_information.txt.gz")
 
 #Set up bigwig metadata
 rna_meta_str1_df = wiggleplotrConstructMetadata(assays(se_featureCounts)$counts, 
@@ -52,6 +59,14 @@ coverage_plot = plotCoverage(exons = tx_ranges,
                              return_subplots_list = FALSE, coverage_type = "line")
 ggsave("results/figures/IRF5_promoter_coverage.pdf", plot = coverage_plot, width = 6, height = 7)
 
+#Make a boxplot
+boxplot = constructQtlPlotDataFrame("ENSG00000128604.grp_1.upstream.ENST00000249375", "rs3778754", 
+                                            tpm_matrix, vcf_file$genotypes, sample_meta, gene_meta) %>% 
+  dplyr::filter(condition_name %in% c("naive")) %>%
+  dplyr::left_join(constructGenotypeText("rs3778754", variant_information), by = "genotype_value") %>%
+  plotQtlCol()
+ggsave("results/figures/IRF5_promoter_boxplot.pdf", plot = boxplot, width = 4, height = 4)
+
 
 ###### Contained ######
 txs = dplyr::filter(tx_metadata, position == "contained")
@@ -70,6 +85,15 @@ coverage_plot = plotCoverage(exons = tx_ranges,
                              return_subplots_list = FALSE, coverage_type = "line")
 ggsave("results/figures/IRF5_RI_coverage.pdf", plot = coverage_plot, width = 6, height = 7)
 
+#Make a boxplot
+boxplot = constructQtlPlotDataFrame("ENSG00000128604.grp_1.contained.ENST00000619830", "rs199508964", 
+                                    tpm_matrix, vcf_file$genotypes, sample_meta, gene_meta) %>% 
+  dplyr::filter(condition_name %in% c("naive")) %>%
+  dplyr::left_join(constructGenotypeText("rs199508964", variant_information), by = "genotype_value") %>%
+  plotQtlCol()
+ggsave("results/figures/IRF5_RI_boxplot.pdf", plot = boxplot, width = 4, height = 4)
+
+
 ###### Downstream ######
 txs = dplyr::filter(tx_metadata, position == "downstream")
 tx_ranges = revised_granges[txs$transcript_id]
@@ -87,6 +111,13 @@ coverage_plot = plotCoverage(exons = tx_ranges,
                              return_subplots_list = FALSE, coverage_type = "line")
 ggsave("results/figures/IRF5_UTR_coverage.pdf", plot = coverage_plot, width = 6, height = 5)
 
+#Make a boxplot
+boxplot = constructQtlPlotDataFrame("ENSG00000128604.grp_1.downstream.ENST00000489702", "rs10954213", 
+                                    tpm_matrix, vcf_file$genotypes, sample_meta, gene_meta) %>% 
+  dplyr::filter(condition_name %in% c("naive")) %>%
+  dplyr::left_join(constructGenotypeText("rs10954213", variant_information), by = "genotype_value") %>%
+  plotQtlCol()
+ggsave("results/figures/IRF5_UTR_boxplot.pdf", plot = boxplot, width = 4, height = 4)
 
 
 

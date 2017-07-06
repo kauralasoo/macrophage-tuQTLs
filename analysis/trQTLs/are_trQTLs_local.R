6c5b19ba522dc42d296abfef6b63c651c66f8473
Kaur Alasoo <kaur.alasoo@gmail.com>
2017-07-06 15:11:04 +0300

    Recaclulate Pi1 between conditions

--- analysis/trQTLs/are_trQTLs_local.R
+++ analysis/trQTLs/are_trQTLs_local.R
@@ -4,6 +4,34 @@ library("ggplot2")
 library("SummarizedExperiment")
 load_all("../seqUtils/")
 
+#Functions
+calculatePositionPi1 <- function(p_matrix, p_threshold = 1e-5){
+  
+  up_down = dplyr::filter(p_matrix, upstream < p_threshold, !is.na(downstream))
+  p1 = 1 - qvalue::qvalue(up_down$downstream)$pi0
+  
+  up_contained = dplyr::filter(p_matrix, upstream < p_threshold, !is.na(contained))
+  p2 = 1 - qvalue::qvalue(up_contained$contained)$pi0
+  
+  down_contained = dplyr::filter(p_matrix, downstream < p_threshold, !is.na(contained))
+  p3 = 1 - qvalue::qvalue(down_contained$contained)$pi0
+  
+  down_up = dplyr::filter(p_matrix, downstream < p_threshold, !is.na(upstream))
+  p4 = 1 - qvalue::qvalue(down_up$upstream)$pi0
+  
+  contained_up = dplyr::filter(p_matrix, contained < p_threshold, !is.na(upstream))
+  p5 = 1 - qvalue::qvalue(contained_up$upstream)$pi0
+  
+  contained_down = dplyr::filter(p_matrix, contained < p_threshold, !is.na(downstream))
+  p6 = 1 - qvalue::qvalue(contained_down$downstream)$pi0
+  
+  #Combine into df
+  result = dplyr::data_frame(first = c("start","start","end","end","mid","mid"),
+                             second = c("end","mid","mid","start","start","end"),
+                             pi1 = c(p1,p2,p3,p4,p5,p6))
+  return(result)
+}
+
 #Import SummarizedExperiments
 se_revised = readRDS("results/SummarizedExperiments/salmonella_salmon_reviseAnnotations.rds")
 se_leafcutter = readRDS("results/SummarizedExperiments/salmonella_leafcutter_counts.rds")
@@ -20,63 +48,62 @@ ensembl_names = tbl_df2(rowData(se_ensembl)) %>%
 #Import QTLs
 salmonella_qtls = readRDS("results/trQTLs/salmonella_trQTL_min_pvalues.rds")
 
-#Identify all genes with qtls
-qtl_genes = dplyr::left_join(salmonella_qtls$reviseAnnotations$naive, revised_names, by = "phenotype_id") %>%
-  dplyr::filter(p_fdr < 0.1) %>%
-  dplyr::select(gene_id) %>%
-  unique()
 
-#naive QTLs
-qtls = dplyr::left_join(salmonella_qtls$reviseAnnotations_groupwise$naive, revised_names, by = "phenotype_id") %>%
-  tidyr::separate("group_id", c("gene1","group_name","position"), sep = "\\.", remove = FALSE) %>%
-  dplyr::select(-gene1)
+#### reviseAnnotations ####
+revised_qtls = salmonella_qtls$reviseAnnotations_groupwise[1:4]
 
-ifng_qtls = dplyr::left_join(salmonella_qtls$reviseAnnotations_groupwise$IFNg, revised_names, by = "phenotype_id") %>%
-  tidyr::separate("group_id", c("gene1","group_name","position"), sep = "\\.", remove = FALSE) %>%
-  dplyr::select(-gene1)
+#Add names to QTLs
+named_qtls = purrr::map(revised_qtls, ~dplyr::left_join(.,revised_names, by = "phenotype_id") %>%
+                        tidyr::separate("group_id", c("gene1","group_name","position"), sep = "\\.", remove = FALSE) %>%
+                        dplyr::select(-gene1))
 
+#Identify best group in each condition
 #Count QTLs per group
-best_group = dplyr::group_by(qtls, gene_id, gene_name, group_name) %>% 
+best_group = purrr::map(named_qtls, ~dplyr::group_by(., gene_id, gene_name, group_name) %>% 
   dplyr::arrange(phenotype_id) %>%
   dplyr::filter(p_fdr < 0.1) %>% 
   dplyr::summarize(qtl_count = length(phenotype_id), min_p = min(p_beta)) %>% 
-  ungroup() %>% dplyr::group_by(gene_id) %>% 
-  arrange(gene_id, -qtl_count) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::group_by(gene_id) %>% 
+  dplyr::arrange(gene_id, -qtl_count) %>% 
   dplyr::filter(row_number() == 1) %>% 
-  dplyr::ungroup()
+  dplyr::ungroup())
 
-best_filtered = dplyr::semi_join(qtls, best_group, by = c("gene_id", "group_name"))
+#Filter best groups in each condition
+best_filtered = purrr::map2(named_qtls, best_group, ~dplyr::semi_join(.x,.y, by = c("gene_id","group_name")))
 
-#Perform Pi1 analysis
-p_matrix = dplyr::select(best_filtered, gene_id, gene_name, position, p_beta) %>% 
-  tidyr::spread(position, p_beta)
+#### Pi1 analysis between positions ####
+#Contruct a matrix of p-values for each position
+p_matrix_list = purrr::map(best_filtered, ~dplyr::select(., gene_id, gene_name, position, p_beta) %>% 
+  tidyr::spread(position, p_beta))
 
-up_down = dplyr::filter(p_matrix, upstream < 1e-5, !is.na(downstream))
-1 - qvalue::qvalue(up_down$downstream)$pi0
+#Calculate pi1 between all positions in each condition
+position_pi1_df = purrr::map_df(p_matrix_list, calculatePositionPi1, .id = "condition_name") %>%
+  dplyr::mutate(comparison = paste(first, second, sep = "_"))
 
-up_contained = dplyr::filter(p_matrix, upstream < 1e-5, !is.na(contained))
-1 - qvalue::qvalue(up_contained$contained)$pi0
-
-down_contained = dplyr::filter(p_matrix, downstream < 1e-5, !is.na(contained))
-1 - qvalue::qvalue(down_contained$contained)$pi0
-
-down_up = dplyr::filter(p_matrix, downstream < 1e-5, !is.na(upstream))
-1 - qvalue::qvalue(down_up$upstream)$pi0
-
-contained_up = dplyr::filter(p_matrix, contained < 1e-5, !is.na(upstream))
-1 - qvalue::qvalue(contained_up$upstream)$pi0
-
-contained_down = dplyr::filter(p_matrix, contained < 1e-5, !is.na(downstream))
-1 - qvalue::qvalue(contained_down$downstream)$pi0
+#Make a boxplot
+pi1_positions_plot = ggplot(position_pi1_df, aes(x = comparison, y = pi1)) + 
+  geom_boxplot() +
+  geom_point() + 
+  scale_y_continuous(limits = c(0,1))
+ggsave("results/figures/pi1_position_plot.pdf",plot = pi1_positions_plot, width = 5, height = 4)
 
 
-#Perform Pi1 between conditions
-best_ifng = dplyr::semi_join(ifng_qtls, best_group, by = c("gene_id", "group_name"))
+#### Pi1 analysis between conditions ####
+all_hits = purrr::map(best_filtered, ~dplyr::filter(.,p_beta < 1e-5))
+others = purrr::map(named_qtls, ~dplyr::semi_join(., all_hits$naive, by = "group_id"))
+naive_pi1 = purrr::map_df(others[2:4], ~data_frame(first = "naive", pi1 = 1 - qvalue::qvalue(.$p_beta)$pi0), .id = "second")
 
-naive_hits = dplyr::filter(best_filtered, p_beta < 1e-5)
-ifng_hits = dplyr::semi_join(ifng_qtls, naive_hits, by = "group_id")
-1 - qvalue::qvalue(ifng_hits$p_beta)$pi0
+others = purrr::map(named_qtls, ~dplyr::semi_join(., all_hits$IFNg, by = "group_id"))
+ifng_pi1 = purrr::map_df(others[c(1,3,4)], ~data_frame(first = "IFNg", pi1 = 1 - qvalue::qvalue(.$p_beta)$pi0), .id = "second")
 
+others = purrr::map(named_qtls, ~dplyr::semi_join(., all_hits$SL1344, by = "group_id"))
+sl1344_pi1 = purrr::map_df(others[c(1,2,4)], ~data_frame(first = "SL1344", pi1 = 1 - qvalue::qvalue(.$p_beta)$pi0), .id = "second")
+
+others = purrr::map(named_qtls, ~dplyr::semi_join(., all_hits$IFNg_SL1344, by = "group_id"))
+IFNg_SL1344_pi1 = purrr::map_df(others[c(1,2,3)], ~data_frame(first = "IFNg_SL1344", pi1 = 1 - qvalue::qvalue(.$p_beta)$pi0), .id = "second")
+
+condition_pi1_df = dplyr::bind_rows(naive_pi1, ifng_pi1, sl1344_pi1, IFNg_SL1344_pi1)
 
 
 

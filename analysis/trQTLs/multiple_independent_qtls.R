a35b77baf2041ff9aac2c92e8593afadd54c5646
Kaur Alasoo <kaur.alasoo@gmail.com>
2017-07-18 12:02:28 +0300

    Count the number of independent QTLs

--- analysis/trQTLs/multiple_independent_qtls.R
+++ analysis/trQTLs/multiple_independent_qtls.R
@@ -1,6 +1,7 @@
 library("dplyr")
 library("devtools")
 library("ggplot2")
+library("SummarizedExperiment")
 load_all("../seqUtils/")
 
 #Functions
@@ -29,6 +30,55 @@ countMultigeneQTLs <- function(pvalues_df, gene_name_map, fdr_threshold = 0.1, R
   return(list(R2 = revised_multigene, ratio = ratio))
 }
 
+countIndependentQTLs <- function(pvalues_list, gene_name_map, fdr_threshold = 0.1, R2_threshold = 0.2){
+  
+  #Extract QTLs
+  qtls_list = purrr::map(pvalues_list, ~dplyr::filter(.,p_fdr < fdr_threshold) %>% 
+                           dplyr::left_join(gene_name_map, by = "phenotype_id") %>%
+                           dplyr::select(group_id, phenotype_id, gene_name, gene_id, p_nominal, snp_id) %>%
+                           dplyr::arrange(gene_id, p_nominal) %>%
+                           dplyr::group_by(gene_id) %>%
+                           dplyr::mutate(qtl_count = length(gene_id)) %>%
+                           dplyr::ungroup())
+  
+  #Count unique genes
+  total_gene_count = purrr::map_df(qtls_list, identity, .id = "condition_name") %>%
+    dplyr::select(gene_id, condition_name) %>%
+    unique() %>%
+    dplyr::group_by(condition_name) %>%
+    dplyr::summarize(total_gene_count = length(gene_id))
+  
+  #Count multiple assoc for gene
+  multi_gene_count = purrr::map_df(qtls_list, identity, .id = "condition_name") %>%
+    dplyr::filter(qtl_count > 1) %>%
+    dplyr::select(gene_id, condition_name) %>%
+    unique() %>%
+    dplyr::group_by(condition_name) %>%
+    dplyr::summarize(multi_gene_count = length(gene_id))
+  
+  #Find indenpednent qtls
+  independent_qtls = purrr::map(qtls_list, ~dplyr::select(., gene_id, snp_id, gene_name, phenotype_id) %>% 
+                                  filterHitsR2(vcf_file$genotypes, R2_thresh = R2_threshold) %>% 
+                                  dplyr::group_by(gene_id) %>%
+                                  dplyr::mutate(qtl_count = length(gene_id)) %>%
+                                  dplyr::ungroup() %>%
+                                  dplyr::filter(qtl_count > 1) %>%
+                                  dplyr::arrange(-qtl_count))
+  
+  independent_qtl_count = purrr::map_df(independent_qtls, identity, .id = "condition_name") %>%
+    dplyr::select(gene_id, condition_name) %>%
+    unique() %>%
+    dplyr::group_by(condition_name) %>%
+    dplyr::summarize(independent_gene_count = length(gene_id))
+  
+  multi_counts = dplyr::left_join(total_gene_count, multi_gene_count, by = "condition_name") %>%
+    dplyr::left_join(independent_qtl_count, by = "condition_name") %>%
+    dplyr::mutate(percent_all = independent_gene_count/total_gene_count) %>%
+    dplyr::mutate(percent_multi = independent_gene_count/multi_gene_count)
+  
+  return(multi_counts)
+}
+
 #Import SummarizedExperiments
 se_revised = readRDS("results/SummarizedExperiments/salmonella_salmon_reviseAnnotations.rds")
 se_leafcutter = readRDS("results/SummarizedExperiments/salmonella_leafcutter_counts.rds")
@@ -54,53 +104,29 @@ salmonella_qtls = readRDS("results/trQTLs/salmonella_trQTL_min_pvalues.rds")
 
 ##### reviseAnnotations #####
 #Identify genes with multiple independent QTLs
-qtls = purrr::map_df(salmonella_qtls$reviseAnnotations[1:4], identity, .id = "condition_name") %>%
-  dplyr::filter(p_fdr < 0.025) %>% 
-  dplyr::left_join(revised_names, by = "phenotype_id") %>%
-  dplyr::select(group_id, phenotype_id, gene_name, gene_id, p_nominal, snp_id) %>%
-  dplyr::arrange(gene_id, p_nominal) %>%
-  dplyr::group_by(gene_id) %>%
-  dplyr::mutate(qtl_count = length(gene_id)) %>%
-  dplyr::ungroup() 
-
-#Find indenpednent qtls
-independent_qtls = dplyr::select(qtls, gene_id, snp_id, gene_name, phenotype_id) %>% 
-  filterHitsR2(vcf_file$genotypes, R2_thresh = .2) %>% 
-  dplyr::group_by(gene_id) %>%
-  dplyr::mutate(qtl_count = length(gene_id)) %>%
-  dplyr::ungroup() %>%
-  dplyr::filter(qtl_count > 1) %>%
-  dplyr::arrange(-qtl_count)
-
-a = length(unique(qtls$gene_id))
-b = length(unique(independent_qtls$gene_id))
-b/a
+revised_counts_02 = countIndependentQTLs(salmonella_qtls$reviseAnnotations[1:4], revised_names,
+                                                  fdr_threshold = 0.1, R2_threshold = 0.2) %>%
+  dplyr::mutate(R2 = 0.2)
+revised_counts_08 = countIndependentQTLs(salmonella_qtls$reviseAnnotations[1:4], revised_names,
+                                      fdr_threshold = 0.1, R2_threshold = 0.8) %>%
+  dplyr::mutate(R2 = 0.8)
+revised_counts = dplyr::bind_rows(revised_counts_02,revised_counts_08)
+write.table(revised_counts, "results/figures/tables/revised_independent_qtls.txt", 
+            sep = "\t", quote = FALSE, row.names = FALSE)
 
 #### leafCutter ####
-#Identify genes with multiple independent QTLs
-qtls = purrr::map_df(salmonella_qtls$leafcutter[1:4], identity, .id = "condition_name") %>% 
-  dplyr::filter(p_fdr < 0.025) %>% 
-  dplyr::left_join(leafcutter_names, by = "phenotype_id") %>%
-  dplyr::select(group_id, phenotype_id, gene_name, gene_id, p_nominal, snp_id) %>%
-  dplyr::arrange(gene_id, p_nominal) %>%
-  dplyr::group_by(gene_id) %>%
-  dplyr::mutate(qtl_count = length(gene_id)) %>%
-  dplyr::ungroup() %>%
-  dplyr::filter(!is.na(gene_name))
+leafcutter_counts_02 = countIndependentQTLs(salmonella_qtls$leafcutter[1:4], leafcutter_names,
+                                         fdr_threshold = 0.1, R2_threshold = 0.2) %>%
+  dplyr::mutate(R2 = 0.2)
+leafcutter_counts_08 = countIndependentQTLs(salmonella_qtls$leafcutter[1:4], leafcutter_names,
+                                         fdr_threshold = 0.1, R2_threshold = 0.8) %>%
+  dplyr::mutate(R2 = 0.8)
+leafcutter_counts = dplyr::bind_rows(leafcutter_counts_02,leafcutter_counts_08)
+write.table(leafcutter_counts, "results/figures/tables/leafcutter_independent_qtls.txt", 
+            sep = "\t", quote = FALSE, row.names = FALSE)
 
-#Find indenpednent qtls
-independent_qtls = dplyr::select(qtls, gene_id, snp_id, gene_name, phenotype_id) %>% 
-  filterHitsR2(vcf_file$genotypes, R2_thresh = .2) %>% 
-  dplyr::group_by(gene_id) %>%
-  dplyr::mutate(qtl_count = length(gene_id)) %>%
-  dplyr::ungroup() %>%
-  dplyr::filter(qtl_count > 1) %>%
-  dplyr::arrange(-qtl_count)
 
-a = length(unique(qtls$gene_id))
-b = length(unique(independent_qtls$gene_id))
-b/a
-
+##### Count QTLs that have multiple target genes ####
 
 #Ensembl
 #### leafCutter ####

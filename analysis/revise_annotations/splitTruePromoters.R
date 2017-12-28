2d35f1b26bf9885beb5e1ed831ec639b9e883911
Kaur Alasoo <kaur.alasoo@gmail.com>
2017-12-28 12:07:02 +0200

    Export promoter events as gffs

---
+++ analysis/revise_annotations/splitTruePromoters.R
@@ -0,0 +1,22 @@
+library("rtracklayer")
+library("dplyr")
+library("devtools")
+library("data.table")
+load_all("../txrevise/")
+load_all("../seqUtils/")
+
+new_promoters = rtracklayer::import.gff3("processed/annotations/txrevise_promoters/merged/txrevise_promoters.gff3.gz")
+
+tx_records = as.data.frame(new_promoters) %>% tbl_df() %>%
+  dplyr::filter(type == "mRNA") %>%
+  dplyr::transmute(transcript_id = ID, transcript_start = start, transcript_end = end, 
+                   transcript_length = width, strand = as.character(strand), chr = as.character(seqnames))
+
+#Expract groups
+promoters_grp1 = new_promoters[new_promoters$gene_id %like% "grp_1"]
+promoters_grp2 = new_promoters[new_promoters$gene_id %like% "grp_2"]
+
+#Export
+out_dir = "processed/annotations/gff/"
+rtracklayer::export.gff3(promoters_grp1, file.path(out_dir, "txrevise.grp_1_promoters.gff3"))
+rtracklayer::export.gff3(promoters_grp2, file.path(out_dir, "txrevise.grp_2_promoters.gff3"))

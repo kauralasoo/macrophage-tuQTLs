60d24d3e667eb905f332307e1dc588efed18d060
Kaur Alasoo <kaur.alasoo@gmail.com>
2018-01-16 18:43:48 +0200

    Replace gene ids with numbers

--- scripts/qtltools_to_fgwas.py
+++ scripts/qtltools_to_fgwas.py
@@ -1,11 +1,19 @@
 import scipy.stats as st
 import gzip
+import argparse
 
 #st.norm.ppf(q, loc=0, scale=1)
 
-qtltools_file = gzip.open('test.txt.gz', 'r')
-fgwas_file = gzip.open('processed/annotations/fgwas/fgwas_annotations.txt.gz','r')
-n_samples = '86'
+parser = argparse.ArgumentParser(description = "Convert QTLtools output into format suitable for fgwas.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--qtltools", help = "Sorted QTLtools output file.")
+parser.add_argument("--annot", help = "Variant annotations for fgwas (sorted by positition)")
+parser.add_argument("--N", help = "QTL sample size.")
+args = parser.parse_args()
+
+#Set up input files
+qtltools_file = gzip.open(args.qtltools, 'r')
+fgwas_file = gzip.open(args.annot,'r')
+n_samples = args.N
 
 #Make full header
 header = "SNPID CHR POS Z F N SEGNUMBER"
@@ -19,6 +27,10 @@ current_fgwas_pos = 0
 current_fgwas_line = ""
 current_fgwas_chr = "1"
 
+#Gene dictionary
+gene_dict = dict()
+gene_count = 0
+
 for line in qtltools_file:
     line = line.decode("utf8").rstrip()
     fields = line.split("\t")
@@ -29,6 +41,14 @@ for line in qtltools_file:
     pval = float(fields[11])
     effect = float(fields[12])
     af = '0'
+
+    #Convert gene id to SEGNUMBER
+    if gene_id in gene_dict:
+        gene_number = gene_dict[gene_id]
+    else:
+        gene_count = gene_count + 1  
+        gene_number = gene_count
+        gene_dict[gene_id] = gene_number
 
     #Calculate z-score
     if(effect > 0):
@@ -63,5 +83,5 @@ for line in qtltools_file:
         annots = fgwas_fields[5:]
 
     #Compile the final string
-    string = " ".join([snp_id, snp_chr, str(snp_pos), str(zscore), str(af), n_samples, gene_id] + annots) 
+    string = " ".join([snp_id, snp_chr, str(snp_pos), str(zscore), str(af), n_samples, str(gene_number)] + annots) 
     print(string)

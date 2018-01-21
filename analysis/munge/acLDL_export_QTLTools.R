72755d61f158dab16999551a00ae43580cd25de6
Kaur Alasoo <kaur.alasoo@gmail.com>
2018-01-21 12:22:54 +0200

    Map QTLs for transcript ends

--- analysis/munge/acLDL_export_QTLTools.R
+++ analysis/munge/acLDL_export_QTLTools.R
@@ -164,6 +164,47 @@ saveFastqtlMatrices(covariates_list, output_path, file_suffix = "covariates_prop
 
 
 
+#Export txrevise end counts for QTLtools
+se_revised = readRDS("results/SummarizedExperiments/acLDL_salmon_txrevise_ends.rds")
+event_metadata = rowData(se_revised)
+unique_genes = unique(event_metadata$gene_id)
+
+#Remove events on X and Y chromosomes
+event_dataset = se_revised[event_metadata[!event_metadata$chr %in% c("X","Y","MT"),]$transcript_id,]
+
+#Extract lists for each condition
+condition_list = idVectorToList(c("Ctrl","AcLDL"))
+event_conditions = purrr::map(condition_list, ~extractConditionFromSummarizedExperiment(.,event_dataset))
+
+#Rename columns
+event_conditions_renamed = purrr::map(event_conditions, function(x){
+  colnames(x) = x$genotype_id
+  return(x)
+})
+
+#Construct gene positions for QTL mapping
+fastqtl_genepos = tbl_df2(rowData(event_dataset)) %>% 
+  dplyr::filter(transcript_id %in% rownames(event_dataset)) %>%
+  constructQTLtoolsGenePos()
+output_path = "processed/acLDL/qtltools/input/txrevise_ends/"
+
+#Extract ratio matrices
+prop_list = purrr::map(event_conditions_renamed, ~assays(.)$tpm_ratios)
+
+#Quantile normalise and remove NAs
+normalised_list = purrr::map(prop_list, ~replaceNAsWithRowMeans(.) %>% quantileNormaliseRows())
+fastqtl_norm_prop_list = purrr::map(normalised_list, prepareQTLtoolsMatrix, fastqtl_genepos)
+saveFastqtlMatrices(fastqtl_norm_prop_list, output_path, file_suffix = "norm_prop")
+
+#Calculate covariates
+sample_meta = tbl_df2(colData(event_conditions_renamed$Ctrl))
+covariates_list = purrr::map(normalised_list, 
+                             ~performPCA(., sample_meta, n_pcs = 6, feature_id = "genotype_id")$pca_matrix %>%
+                               dplyr::select(genotype_id, PC1, PC2, PC3, PC4, PC5, PC6) %>%
+                               fastqtlMetadataToCovariates())
+saveFastqtlMatrices(covariates_list, output_path, file_suffix = "covariates_prop")
+
+
 
 
 #Export LeafCutter counts for QTLtools
---
+++ analysis/munge/acLDL_importTxreviseEnds.R
@@ -0,0 +1,58 @@
+library("dplyr")
+library("readr")
+library("tximport")
+library("devtools")
+library("SummarizedExperiment")
+load_all("../seqUtils/")
+load_all("analysis/housekeeping/")
+
+#Import Ensembl quant results for references
+ensembl_quants = readRDS("results/SummarizedExperiments/acLDL_salmon_Ensembl_87.rds")
+sample_names = colnames(ensembl_quants)
+
+#Import all transcript abundances
+tx_abundances = list('txrevise.grp_1_ends' = readRDS("processed/acLDL/matrices/txrevise.grp_1_ends.salmon_txrevise.rds"),
+                     'txrevise.grp_2_ends' = readRDS("processed/acLDL/matrices/txrevise.grp_2_ends.salmon_txrevise.rds"))
+tx_transposed = purrr::transpose(tx_abundances)
+
+#Extract abundances, counts and lengths
+abundances = purrr::reduce(tx_transposed$abundance, rbind)
+counts = purrr::reduce(tx_transposed$counts, rbind)
+lengths = purrr::reduce(tx_transposed$counts, rbind)
+
+#Extract gene ids from event names
+gene_meta = tbl_df2(rowData(ensembl_quants)) %>%
+  dplyr::select(gene_id, gene_name, chr, strand, start, end) %>%
+  unique()
+gene_names = data_frame(transcript_id = rownames(abundances)) %>% 
+  tidyr::separate(transcript_id, c("ensembl_gene_id", "group", "position", "ensembl_transcript_id"), "\\.", remove = FALSE) %>% 
+  dplyr::mutate(gene_id = paste(ensembl_gene_id, position, sep = ".")) %>%
+  dplyr::mutate(group_id = paste(ensembl_gene_id, group, position, sep = ".")) %>%
+  dplyr::filter(ensembl_gene_id %in% gene_meta$gene_id) %>%
+  dplyr::left_join(gene_meta, gene_meta, by = c("ensembl_gene_id" = "gene_id")) %>%
+  dplyr::mutate(strand = ifelse(strand == 1, "+","-")) %>%
+  dplyr::select(-group, -position) %>%
+  as.data.frame()
+rownames(gene_names) = gene_names$transcript_id
+
+#Filter quants
+abundances_filtered = abundances[gene_names$transcript_id, colData(ensembl_quants)$sample_id]
+counts_filtered = counts[gene_names$transcript_id, colData(ensembl_quants)$sample_id]
+lengths_filtered = lengths[gene_names$transcript_id, colData(ensembl_quants)$sample_id]
+
+
+#Calculate abundance ratios
+gene_name_map = dplyr::transmute(gene_names, gene_id = group_id, transcript_id)
+abundance_ratios = calculateTranscriptRatios(abundances_filtered, gene_name_map)
+
+#Construct a SummarizedExperiment object
+se = SummarizedExperiment::SummarizedExperiment(
+  assays = list(counts = counts_filtered, tpms = abundances_filtered, 
+                relLengths = lengths_filtered, tpm_ratios = abundance_ratios), 
+  colData = colData(ensembl_quants), 
+  rowData = gene_names)
+
+saveRDS(se, "results/SummarizedExperiments/acLDL_salmon_txrevise_ends.rds")
+
+
+

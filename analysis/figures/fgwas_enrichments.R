81d7f76a9dc8e02012a0973d1993401e230e342d
Kaur Alasoo <kaur.alasoo@gmail.com>
2018-05-10 01:15:18 +0300

    Repeat fgwas analysis on individual models.

--- analysis/figures/fgwas_enrichments.R
+++ analysis/figures/fgwas_enrichments.R
@@ -10,7 +10,7 @@ load_all("analysis/housekeeping/")
 file_list = list.files("processed/salmonella/fgwas/output/", recursive = T)
 
 df = data_frame(file_path = file_list[grep("params", file_list)]) %>%
-  tidyr::separate(file_path, c("quant", "cond_string"), sep ="\\/", remove = F) %>% 
+  tidyr::separate(file_path, c("model","quant", "cond_string"), sep ="\\/", remove = F) %>% 
   tidyr::separate(cond_string, c("condition_name","empty"), sep = ".params" ) %>%
   dplyr::select(-empty) %>%
   dplyr::mutate(full_path = file.path("processed/salmonella/fgwas/output", file_path))
@@ -33,6 +33,7 @@ friendly_parameters = data_frame(
 file_paths = setNames(df$full_path, df$file_path)
 all_data = purrr::map_df(file_paths, ~readr::read_delim(., delim = " ", col_types = "cddd"), .id = "file_path") %>%
   dplyr::mutate(CI_hi = ifelse(is.na(CI_hi), estimate + (estimate-CI_lo), CI_hi)) %>%
+  dplyr::mutate(CI_lo = ifelse(is.na(CI_lo), estimate - (CI_hi-estimate), CI_lo)) %>%
   dplyr::left_join(df, by = "file_path") %>%
   left_join(conditionFriendlyNames()) %>%
   left_join(phenotypeFriendlyNames()) %>%
@@ -40,7 +41,8 @@ all_data = purrr::map_df(file_paths, ~readr::read_delim(., delim = " ", col_type
   dplyr::left_join(friendly_parameters)
 
 ### Compare different quant methods
-data = dplyr::filter(all_data, figure_name == "I", !is.na(phenotype)) %>%
+joint_model_data = dplyr::filter(all_data, model == "atac_peak+polyA_site+fiveUTR+threeUTR+CDS+intron+promoter+eCLIP_3end+eCLIP_splicing+eCLIP_both")
+data = dplyr::filter(joint_model_data, figure_name == "N", !is.na(phenotype)) %>%
   dplyr::filter(quant %in% c("Ensembl_87", "featureCounts","leafcutter","reviseAnnotations"))
 
 methods_plot = ggplot(data, aes(x = estimate, y = phenotype, xmin = CI_lo, xmax = CI_hi)) + 
@@ -57,7 +59,7 @@ ggsave("results/figures/fgwas_methods.pdf", plot = methods_plot, width = 3.2, he
 
 
 #Compare different positioms
-data = dplyr::filter(all_data, figure_name == "I", quant %in% 
+data = dplyr::filter(joint_model_data, figure_name == "N", quant %in% 
                        c("txrevise_contained","txrevise_ends","txrevise_promoters","leafcutter"))
 
 methods_plot = ggplot(data, aes(x = estimate, y = phenotype, xmin = CI_lo, xmax = CI_hi)) + 
@@ -68,6 +70,41 @@ methods_plot = ggplot(data, aes(x = estimate, y = phenotype, xmin = CI_lo, xmax 
   xlab("Log(enrichment)") +
   theme(strip.text.y = element_text(angle = 0)) + 
   coord_cartesian(xlim = c(-.2,3.9)) +
+  theme(panel.spacing = unit(0.1, "lines"))
+ggsave("results/figures/fgwas_position.pdf", plot = methods_plot, width = 3.2, height = 4)
+
+
+
+#Repeat the analysis for individual models
+
+### Compare different quant methods
+indiv_model_data = dplyr::filter(all_data, model != "atac_peak+polyA_site+fiveUTR+threeUTR+CDS+intron+promoter+eCLIP_3end+eCLIP_splicing+eCLIP_both")
+data = dplyr::filter(indiv_model_data, figure_name == "I", !is.na(phenotype)) %>%
+  dplyr::filter(quant %in% c("Ensembl_87", "featureCounts","leafcutter","reviseAnnotations"))
+
+methods_plot = ggplot(data, aes(x = estimate, y = phenotype, xmin = CI_lo, xmax = CI_hi)) + 
+  geom_point() + facet_grid(parameter_name~.) +
+  geom_errorbarh(aes(height = 0)) +
+  theme_light() +
+  theme(axis.title.y = element_blank()) +
+  xlab("Log(enrichment)") +
+  theme(strip.text.y = element_text(angle = 0)) + 
+  coord_cartesian(xlim = c(-.2,4.5)) +
+  theme(panel.spacing = unit(0.1, "lines"))
+
+
+#Compare different positioms
+data = dplyr::filter(indiv_model_data, figure_name == "I", quant %in% 
+                       c("txrevise_contained","txrevise_ends","txrevise_promoters","leafcutter"))
+
+methods_plot = ggplot(data, aes(x = estimate, y = phenotype, xmin = CI_lo, xmax = CI_hi)) + 
+  geom_point() + facet_grid(parameter_name~.) +
+  geom_errorbarh(aes(height = 0)) +
+  theme_light() +
+  theme(axis.title.y = element_blank()) +
+  xlab("Log(enrichment)") +
+  theme(strip.text.y = element_text(angle = 0)) + 
+  coord_cartesian(xlim = c(-.2,4.5)) +
   theme(panel.spacing = unit(0.1, "lines"))
 ggsave("results/figures/fgwas_position.pdf", plot = methods_plot, width = 3.2, height = 4)
 

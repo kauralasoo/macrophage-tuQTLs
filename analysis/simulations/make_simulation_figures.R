aedafb2e707fa616b1ffa07b51295ca691d4f845
Kaur Alasoo <kaur.alasoo@gmail.com>
2018-07-13 01:02:18 +0300

    Make more figures and figure out where the false positives come from

--- analysis/simulations/make_simulation_figures.R
+++ analysis/simulations/make_simulation_figures.R
@@ -77,16 +77,107 @@ ggplot(extended_effects, aes(x = estimate, fill = DEstatus)) + geom_histogram()
 
 
 
-#Perform tests for txrevise results
+#### Perform tests for txrevise results
+# Import quantification results
+upstream_original = readRDS("processed/sim_original/matrices/txrevise_upstream.salmon_txrevise.rds")
+downstream_original = readRDS("processed/sim_original/matrices/txrevise_downstream.salmon_txrevise.rds")
+
+#Construct event annotations
+upstream_events = data_frame(transcript_id = rownames(upstream_original$abundance)) %>%
+  tidyr::separate(transcript_id, c("gene_id", "suffix"), sep = "\\.grp_1", remove = FALSE) %>%
+  dplyr::select(-suffix)
+
+downstream_events = data_frame(transcript_id = rownames(downstream_original$abundance)) %>%
+  tidyr::separate(transcript_id, c("gene_id", "suffix"), sep = "\\.grp_1", remove = FALSE) %>%
+  dplyr::select(-suffix) %>%
+  dplyr::group_by(gene_id) %>% 
+  dplyr::filter(row_number() == 1) %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(event_type = "3'end")
+
+selected_events = dplyr::bind_rows(dplyr::mutate(upstream_events, event_type = "promoter"), 
+                                   dplyr::mutate(downstream_events, event_type = "3'end")) %>%
+  dplyr::group_by(gene_id, event_type) %>% 
+  dplyr::filter(row_number() == 1) %>%
+  dplyr::ungroup() %>%
+  dplyr::left_join(lead_variants, by = "gene_id") %>%
+  dplyr:::left_join( dplyr::select(selected_transcripts, gene_id, DEstatus), by = "gene_id")
+
+#Calculate event ratios
+tu_up_original = calculateTranscriptRatios(upstream_original$abundance, upstream_events)
+tu_down_original = calculateTranscriptRatios(downstream_original$abundance, downstream_events)
+event_usage = rbind(tu_up_original, tu_down_original)[selected_events$transcript_id,]
+
+#Run linear model
+snps_df = dplyr::transmute(selected_events, phenotype_id = transcript_id, snp_id)
+results = testMultipleInteractions(snps_df, event_usage, sample_metadata, vcf_file, qtl_formula, 
+                                   interaction_formula, return_value = "all", id_field_separator = "-", lme4 = FALSE)
+original_effects = purrr::map_df(results, ~broom::tidy(.$qtl_model), .id = "test_id") %>% 
+  tidyr::separate(test_id, c("transcript_id", "snp_id"), sep = "-") %>% 
+  dplyr::filter(term == "genotype") %>%
+  dplyr::left_join(selected_events, by = c("transcript_id", "snp_id")) %>%
+  as_tibble() %>%
+  dplyr::mutate(p_fdr = p.adjust(p.value, method = "fdr")) %>% 
+  dplyr::mutate(estimate = abs(estimate))
+
+ggplot(original_effects, aes(x = p.value, fill = DEstatus)) + geom_histogram()
+ggplot(original_effects, aes(x = estimate, fill = DEstatus)) + geom_histogram()
 
 
-a = testInteraction(gene_id = "ENST00000514717", snp_id = "rs898455", tu_extended, sample_metadata, vcf_file, qtl_formula, interaction_formula, return_value = "all")
+#repeat for extended transcripts
+upstream_extended = readRDS("processed/sim_extended/matrices/txrevise_upstream.salmon_txrevise.rds")
+downstream_extended = readRDS("processed/sim_extended/matrices/txrevise_downstream.salmon_txrevise.rds")
+
+#Calculate event ratios
+tu_up_original = calculateTranscriptRatios(upstream_extended$abundance, upstream_events)
+tu_down_original = calculateTranscriptRatios(downstream_extended$abundance, downstream_events)
+event_usage = rbind(tu_up_original, tu_down_original)[selected_events$transcript_id,]
+
+#Run linear model
+snps_df = dplyr::transmute(selected_events, phenotype_id = transcript_id, snp_id)
+results = testMultipleInteractions(snps_df, event_usage, sample_metadata, vcf_file, qtl_formula, 
+                                   interaction_formula, return_value = "all", id_field_separator = "-", lme4 = FALSE)
+extended_effects = purrr::map_df(results, ~broom::tidy(.$qtl_model), .id = "test_id") %>% 
+  tidyr::separate(test_id, c("transcript_id", "snp_id"), sep = "-") %>% 
+  dplyr::filter(term == "genotype") %>%
+  dplyr::left_join(selected_events, by = c("transcript_id", "snp_id")) %>%
+  as_tibble() %>%
+  dplyr::mutate(p_fdr = p.adjust(p.value, method = "fdr")) %>% 
+  dplyr::mutate(estimate = abs(estimate))
+
+ggplot(extended_effects, aes(x = p.value, fill = DEstatus)) + geom_histogram()
+ggplot(extended_effects, aes(x = estimate, fill = DEstatus)) + geom_histogram()
+
+#Identify truncated events
+gene_diffs = dplyr::left_join(tx_diffs, gene_transcript_map, by = c("full_tx" = "transcript_id")) %>%
+  dplyr::select(gene_id, upstream, downstream, truncation) %>%
+  dplyr::filter(truncation != "both") %>%
+  tidyr::gather(position, diff_length, upstream:downstream) %>%
+  dplyr::mutate(event_type = ifelse(position == "upstream", "promoter", "3'end")) %>%
+  dplyr::mutate(truncation = ifelse(truncation == "start", "promoter", "3'end")) %>%
+  dplyr::mutate(is_truncated = ifelse(event_type == truncation, TRUE, FALSE)) %>%
+  dplyr::filter(diff_length > 100) %>%
+  dplyr::select(gene_id, diff_length, event_type, is_truncated)
+
+extended_by_truncation = dplyr::left_join(gene_diffs, extended_effects, by = c("gene_id", "event_type"))
+
+ggplot(extended_by_truncation, aes(x = estimate, fill = DEstatus)) + geom_histogram() + facet_grid(event_type~is_truncated)
+ggplot(extended_by_truncation, aes(x = p.value, fill = DEstatus)) + geom_histogram() + facet_grid(event_type~is_truncated)
+
+View(dplyr::filter(extended_by_truncation, event_type == "promoter", is_truncated == TRUE, DEstatus == TRUE))
 
 
+plotTranscripts(exons[dplyr::filter(gene_transcript_map, gene_id == "ENSG00000033867")$transcript_id])
+dplyr::filter(tx_diffs, truncated_tx == "ENST00000419036")
 
-dplyr::filter(tx_meta, ensembl_transcript_id == "ENST00000514717")
-original_transcripts = readRDS("processed/sim_extended/matrices/txrevise_downstream.salmon_txrevise.rds")
-abundances_mat = original_transcripts$abundance
-which(rownames(abundances_mat) %like% "ENSG00000006283")
+alt_events = readRDS("results/simulations/extended_tx_and_events.rds")
+plotTranscripts(alt_events$ENSG00000033867$alt_events$ENSG00000033867.grp_1$upstream)
+plotTranscripts(alt_events$ENSG00000033867$extended_tx$exons)
+
+
+> dplyr::filter(original_effects, transcript_id == "ENSG00000006283.grp_1.upstream.ENST00000514717")
+> dplyr::filter(extended_effects, transcript_id == "ENSG00000006283.grp_1.upstream.ENST00000514717")
+
+
 
 

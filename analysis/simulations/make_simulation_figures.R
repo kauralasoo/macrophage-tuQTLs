1c266e3e15ea7da9da2c6dcd0b4c13542e2e9bf2
Kaur Alasoo <kaur.alasoo@gmail.com>
2018-07-27 17:08:42 +0300

    Perform DEXseq analysis

--- analysis/simulations/make_simulation_figures.R
+++ analysis/simulations/make_simulation_figures.R
@@ -10,12 +10,6 @@ all_tx = c(tx_diffs$full_tx, tx_diffs$truncated_tx)
 gene_transcript_map = dplyr::transmute(tx_meta, gene_id = ensembl_gene_id, transcript_id = ensembl_transcript_id) %>%
   dplyr::filter(transcript_id %in% all_tx)
 
-#extended_tx_diffs = readRDS("results/simulations/extended_transcript_diffs.rds") %>% 
-#  dplyr::select(full_tx, truncated_tx, truncation)
-#tx_diffs = readRDS("results/simulations/transcript_diffs.rds") %>% 
-#  dplyr::select(-truncation) %>%
-#  dplyr::left_join(extended_tx_diffs)
-
 #Import QTLs
 vcf_file = readRDS("results/genotypes/salmonella/imputed.86_samples.sorted.filtered.named.rds")
 salmonella_qtls = readRDS("results/trQTLs/salmonella_trQTL_min_pvalues.rds")
@@ -25,7 +19,7 @@ lead_variants = dplyr::filter(salmonella_qtls$Ensembl_87$naive, group_id %in% tx
   dplyr::transmute(gene_id = group_id, snp_id)
 
 #Import DEstatus
-design = read.table("results/simulations/one_transcripts/sim_tx_info.txt", header = TRUE)
+design = read.table("results/simulations/m250_sd25/one_transcripts/sim_tx_info.txt", header = TRUE)
 de_status = dplyr::transmute(design, transcript_id = transcriptid, DEstatus = rowSums(design[,88:173])) %>% as_tibble() %>%
   dplyr::mutate(DEstatus = ifelse(DEstatus > 0, TRUE, FALSE))
 
@@ -227,4 +221,118 @@ ggplot(plot_data, aes(x = genotype_value, y = norm_exp)) + geom_boxplot() + geom
 #Option 1: Align to geneome first with hisat2, then use salmon (or RSEM) to quantify
 #Option 2: Construct alternative event annotations that contain 
 
+
+
+
+#### DEXSeq analysis ####
+library("DEXSeq")
+count_files = list.files("processed/sim_one/DEXseq/", full.names = TRUE)
+sampleTable = data.frame(row.names = paste0("sample_", c(1:86)),
+                         condition = c(rep("A", 43), rep("B",43)))
+flattened_file = "results/simulations/both_transcripts.flat.gtf"
+
+dxd = DEXSeqDataSetFromHTSeq(
+  count_files,
+  sampleData=sampleTable,
+  design= ~ sample + exon + condition:exon,
+  flattenedfile= flattened_file )
+
+gtf = rtracklayer::import.gff2(flattened_file)
+exon_metadata = as.data.frame(gtf[gtf$type == "exonic_part",]) %>%
+  as_tibble()
+
+selected_metadata = dplyr::select(exon_metadata, gene_id, strand, exonic_part_number, width, transcripts) %>% 
+  dplyr::mutate(is_shared = ifelse(transcripts %like% '\\+', T, F)) %>% 
+  dplyr::mutate(exon_rank = as.numeric(exonic_part_number))
+min_shared_rank = dplyr::filter(selected_metadata, is_shared) %>% dplyr::group_by(gene_id) %>% dplyr::summarize(min_shared_rank = min(exon_rank))
+
+event_assignment = dplyr::left_join(selected_metadata, min_shared_rank) %>% 
+  dplyr::mutate(event_type = case_when(
+    !(is_shared) & (exon_rank < min_shared_rank) & strand == "+" ~ "promoter",
+    !(is_shared) & (exon_rank < min_shared_rank) & strand == "-" ~ "3'end",
+    !(is_shared) & (exon_rank > min_shared_rank) & strand == "+" ~ "3'end",
+    !(is_shared) & (exon_rank > min_shared_rank) & strand == "-" ~ "promoter",
+    is_shared ~ "shared"
+    )) %>%
+  dplyr::mutate(exon_id = paste0("E", exonic_part_number),
+                event_id = paste(gene_id, event_type, sep = "_"))
+
+#Link features to events
+feature_event_map = dplyr::transmute(event_assignment, feature_id = paste(gene_id, exon_id, sep = ":"), event_id)
+event_gene_map = dplyr::select(event_assignment, event_id, gene_id, event_type) %>% dplyr::distinct()
+event_lengths = dplyr::group_by(event_assignment, event_id) %>% dplyr::summarise(event_length = sum(width))
+
+#Calculate event counts form exon counts
+count_matrix = counts(dxd)[,1:86]
+colnames(count_matrix) = sample_metadata$sample_id
+
+#Add gene ids to transcript expression matrix
+count_df = dplyr::mutate(as.data.frame(count_matrix), feature_id = rownames(count_matrix)) %>%
+  tbl_df() %>%
+  dplyr::left_join(feature_event_map, by = "feature_id") %>%
+  dplyr::select(-feature_id) %>%
+  dplyr::select(event_id, everything())
+
+#Make event count matrix
+event_total_counts = purrrlyr::slice_rows(count_df, "event_id") %>% 
+  purrrlyr::by_slice(~colSums(.) %>% 
+                       t() %>% 
+                       as.data.frame(), .collate = "rows")
+event_count_matrix = dplyr::select(event_total_counts, -event_id) %>%
+  as.matrix()
+row.names(event_count_matrix) = event_total_counts$event_id
+
+#Reorder and normalize by event length
+event_count_matrix = event_count_matrix[event_lengths$event_id,]
+event_quants = event_count_matrix/event_lengths$event_length
+
+#Calculate promoter usage
+pormoter_events = dplyr::filter(event_gene_map, event_type %in% c("promoter", "shared")) %>% dplyr::transmute(transcript_id = event_id, gene_id)
+promoter_ratios = calculateTranscriptRatios(event_quants[pormoter_events$transcript_id,], pormoter_events)
+
+promoter_only = dplyr::filter(event_gene_map, event_type %in% c("promoter"))
+promoter_ratios = promoter_ratios[promoter_only$event_id,]
+
+#Calculate 3'end usage
+end_events = dplyr::filter(event_gene_map, event_type %in% c("3'end", "shared")) %>% dplyr::transmute(transcript_id = event_id, gene_id)
+end_ratios = calculateTranscriptRatios(event_quants[end_events$transcript_id,], end_events)
+
+end_only = dplyr::filter(event_gene_map, event_type %in% c("3'end"))
+end_ratios = end_ratios[end_only$event_id,]
+
+#Perform QTL analysis
+selected_events = dplyr::filter(event_gene_map, event_type %in% c("promoter", "3'end")) %>% 
+  dplyr::left_join(dplyr::select(selected_transcripts, gene_id, snp_id, DEstatus), by = "gene_id")
+event_usage = rbind(promoter_ratios, end_ratios)[selected_events$event_id,]
+
+#Run linear model
+snps_df = dplyr::transmute(selected_events, phenotype_id = event_id, snp_id)
+results = testMultipleInteractions(snps_df, event_usage, sample_metadata, vcf_file, qtl_formula, 
+                                   interaction_formula, return_value = "all", id_field_separator = "-", lme4 = FALSE)
+extended_effects = purrr::map_df(results, ~broom::tidy(.$qtl_model), .id = "test_id") %>% 
+  tidyr::separate(test_id, c("transcript_id", "snp_id"), sep = "-") %>% 
+  dplyr::filter(term == "genotype") %>%
+  dplyr::left_join(dplyr::rename(selected_events, transcript_id = event_id), by = c("transcript_id", "snp_id")) %>%
+  as_tibble() %>%
+  dplyr::mutate(p_fdr = p.adjust(p.value, method = "fdr")) %>% 
+  dplyr::mutate(estimate = abs(estimate))
+
+ggplot(extended_effects, aes(x = p.value, fill = DEstatus)) + geom_histogram()
+ggplot(extended_effects, aes(x = estimate, fill = DEstatus)) + geom_histogram()
+
+#Identify truncated events
+gene_diffs = dplyr::left_join(tx_diffs, gene_transcript_map, by = c("full_tx" = "transcript_id")) %>%
+  dplyr::select(gene_id, upstream, downstream) %>%
+  tidyr::gather(position, diff_length, upstream:downstream) %>% 
+  dplyr::arrange(gene_id) %>%
+  dplyr::mutate(is_truncated = ifelse(diff_length == 0, TRUE, FALSE)) %>%
+  dplyr::mutate(event_type = ifelse(position == "upstream", "promoter", "3'end")) %>%
+  dplyr::select(gene_id, diff_length, event_type, is_truncated)
+
+extended_by_truncation = dplyr::left_join(gene_diffs, extended_effects, by = c("gene_id", "event_type"))
+
+ggplot(extended_by_truncation, aes(x = estimate, fill = DEstatus)) + geom_histogram() + facet_grid(event_type~is_truncated)
+ggplot(extended_by_truncation, aes(x = p.value, fill = DEstatus)) + geom_histogram() + facet_grid(event_type~is_truncated)
+
+View(dplyr::filter(extended_by_truncation, event_type == "3'end", is_truncated == TRUE, DEstatus == TRUE))
 
